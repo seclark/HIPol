86694a8ec710c97b5718985827a2537958f61b71
Susan Clark <susanclark19@gmail.com>
2017-02-05 21:31:25 -0500

    grab thumbnails

--- code/extract_data.py
+++ code/extract_data.py
@@ -64,11 +64,44 @@ def radec_to_xy(ra, dec, w):
     
     return x, y
     
+def make_circular_mask(hdr, center_x, center_y, radius):
+    """
+    inputs: hdr :: header to grab data size from
+            center_x :: x pixel position of center annulus
+            center_y :: y pixel " " "
+            radius   :: radius of desired annulus
+    """
+    yindx, xindx = np.ogrid[-center_y:hdr['NAXIS2'] - center_y, -center_x:hdr['NAXIS1'] - center_x]
+    mask = xindx**2 + yindx**2 <= radius**2
+    
+    return mask
+    
+def plot_thumbnails():
 
-nhidata_fn = "/Volumes/DataDavy/GALFA/DR2/NHIMaps/GALFA-HI_VLSR-036+0037kms_NHImap_noTcut.fits"
-galfa_hdr = fits.getheader(nhidata_fn)
+    nhidata_fn = "/Volumes/DataDavy/GALFA/DR2/NHIMaps/GALFA-HI_VLSR-036+0037kms_NHImap_noTcut.fits"
+    galfa_hdr = fits.getheader(nhidata_fn)
 
-alldata = get_alldata()
-allras = get_data_from_name(alldata, 'ra')
-alldecs = get_data_from_name(alldata, 'dec')
-allpangs = get_data_from_name(alldata, 'pang')
+    alldata = get_alldata()
+    allras = get_data_from_name(alldata, 'ra')
+    alldecs = get_data_from_name(alldata, 'dec')
+    allpangs = get_data_from_name(alldata, 'pang')
+    allnames = get_data_from_name(alldata, 'name')
+    print(allnames)
+
+    allintrht_fn = "/Volumes/DataDavy/GALFA/DR2/FullSkyRHT/new_thetarht_maps/intrht_coadd_974_1069.fits"
+    intrht = fits.getdata(allintrht_fn)
+    
+    nrows = 5
+    ncols = 4
+    fig = plt.figure(facecolor="white")
+    datar = 100
+    
+    for i, (ra, dec) in enumerate(zip(allras, alldecs)):
+        # get x, y points from ra dec
+        w = make_wcs(nhidata_fn)
+        x_center, y_center = radec_to_xy(ra, dec, w)
+        
+        ax = fig.add_subplot(nrows, ncols, i+1)
+        ax.imshow(intrht[y_center-datar:y_center+datar, x_center-datar:x_center+datar])
+
+
